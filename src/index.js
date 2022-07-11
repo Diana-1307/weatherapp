e9019da903120b57c67ddfe05d890be6581d841e
Diana-1307 <belousovadiana130700@gmail.com>
2022-07-11 21:05:24 +0300

    new folders

---
+++ src/index.js
@@ -0,0 +1,133 @@
+//////////
+
+let now = new Date();
+let h3 = document.querySelector("h3");
+
+let currentHours = now.getHours();
+if (currentHours < 10) {
+  currentHours = `0${currentHours}`;
+}
+
+let currentMinutes = now.getMinutes();
+if (currentMinutes < 10) {
+  currentMinutes = `0${currentMinutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let currentDate = now.getDate();
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h3.innerHTML = `${currentDate} ${month}, ${day}, ${currentHours}:${currentMinutes}`;
+
+////////////
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+
+  let currentCity = document.querySelector("#currentCity");
+  currentCity.innerHTML = `${searchInput.value}`;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+let locationButton = document.querySelector("#current-location");
+locationButton.addEventListener("click", getLocation);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", submitFormSearch);
+
+//////////////////
+function showWeather(response) {
+  console.log();
+
+  document.querySelector(".day-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+
+  document.querySelector(
+    ".currentCity"
+  ).innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function submitFormSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+
+function getPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getPosition);
+}
+
+searchCity("Kyiv");
+
+//function celsiusClick(event) {
+//event.preventDefault();
+//let temperatureChange = document.querySelector(".temperatureNumber");
+//temperatureChange.innerHTML = " ";
+//}
+
+//let celsius = document.querySelector(".celsius-link");
+//celsius.addEventListener("click", celsiusClick);
+
+//function fahrenheitClick(event) {
+//event.preventDefault();
+// let temperatureChange = document.querySelector(".temperatureNumber");
+//temperatureChange.innerHTML = " ";
+//}
+
+//let fahrenheit = document.querySelector(".fahrenheit-link");
+////fahrenheit.addEventListener("click", fahrenheitClick);
