5b1ddfb90d60add3eccd5b8b1fd64b44ad40455a
Diana-1307 <belousovadiana130700@gmail.com>
2022-07-23 15:53:01 +0300

    add html-elements to JS + API

--- src/index.js
+++ src/index.js
@@ -44,7 +44,51 @@ let month = months[now.getMonth()];
 
 h3.innerHTML = `${currentDate} ${month}, ${day}, ${currentHours}:${currentMinutes}`;
 
+//////////////
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `  <div class= "row week-weather">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+
+    <div class="col-2">
+      <div class="weayher-forecast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        class="week-image"
+        alt="Weather image"
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperatures-max">18 &deg</span>
+        <span class="weather-forecast-temperatures-min">12 &deg</span>
+      </div>
+    </div>
+   `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 ////////////
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+/////
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
@@ -89,7 +133,9 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
   celsiusTemperature = response.data.main.temp;
+  console.log(response.data);
 }
 
 function searchCity(city) {
